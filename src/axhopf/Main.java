67e6d3911ebd1fba99322d547dbebbfe7b51e835
ashton <ashton@DESKTOP-HOPMU83>
2026-02-27 21:27:54 -0800

    added basic user class

--- src/axhopf/Main.java
+++ src/axhopf/Main.java
@@ -1,10 +1,50 @@
 package axhopf;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//testing
+		Scanner scnr = new Scanner(System.in);
+		String input = "";
+		String firstName;
+		String lastName;
+		int age;
+		
+		System.out.println("Enter Your First Name:");
+		firstName = scnr.next();
+		System.out.println("Enter Your Last Name:");
+		lastName = scnr.next();
+		System.out.println("Enter Your Age:");
+		age = scnr.nextInt();
+		
+		User user = new User(firstName, lastName, age); //user object
+		
+		System.out.println("Welcome, " + firstName + " " + lastName + ", what would you like to do?");
+		
+		input = scnr.next();
+		
+		switch(input)
+		{
+		case "1":
+		
+		//something here
+			
+		break;
+		case "2":
+			
+		//something here
+			
+		break;
+		case "3":
+		
+		//something here
+			
+		break;
+		//etc
+		}
+		
+	
 	}
 
 }
