31e59feeae20025c7757a570692f871de9874e68
ashton <ashton@DESKTOP-HOPMU83>
2026-03-03 00:57:47 -0800

    Organized Mental Health Resources

--- src/axhopf/Main.java
+++ src/axhopf/Main.java
@@ -5,23 +5,30 @@ import java.util.Scanner;
 
 public class Main {
 
+	public static ArrayList<Information> conversion(List<String> data)
+	{
+		ArrayList<Information> list = new ArrayList<>();
+		for(String item : data)
+		{
+			list.add(new Information(item));
+		}
+		
+		return list;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		List<String> ArticlesInfo = List.of("Mental Health America\nhttps://mhanational.org/bipoc-mental-health/", "NAMI Seattle Mental Health Resources\nhttps://namiseattle.org/support-resources/bipoc-mental-health-resources/");
+		List<String> SafeSpaces = List.of("Black Emotional and Mental Health Collective\nhttps://wellness.beam.community/", "BIPOC Adoptees Support\nhttps://bipocadoptees.space/forms/submit-resource");
+		List<String> Providers = List.of("University of Washington BIPOC Resources\nhttps://geography.washington.edu/community-sourced-resources-bipoc-mental-health");
+		ArrayList<Information> ArticleInfo = conversion(ArticlesInfo);
+		ArrayList<Information> SafeSpaceList = conversion(SafeSpaces);
+		ArrayList<Information> ProviderList = conversion(Providers);
 		
-		List<String> mentalHealthList = List.of("https://mhanational.org/bipoc-mental-health/", "https://namiseattle.org/support-resources/bipoc-mental-health-resources/", "https://geography.washington.edu/community-sourced-resources-bipoc-mental-health", "https://wellness.beam.community/", "https://bipocadoptees.space/forms/submit-resource");
-		List<String> mentalHealthDescriptions = List.of("Mental Health America", "NAMI Seattle Mental Health Resources", "University of Washington BIPOC Resources", "Black Emotional and Mental Health Collective", "BIPOC Adoptees Support");
-		
-		ArrayList<Information> mentalHealthTotal = new ArrayList<Information>();
-		
-		for(int i = 0; i < mentalHealthList.size() && i < mentalHealthDescriptions.size(); i++)
-		{
-			Information info = new Information(mentalHealthList.get(i), mentalHealthDescriptions.get(i)); //creates information objects of mental health resources
-			mentalHealthTotal.add(info);
-		}
-			
 		Information object = new Information();
 		Scanner scnr = new Scanner(System.in);
 		String input = "";
+		String resourceInput = "";
 		String firstName; //user info
 		String lastName;
 		int age;
@@ -35,16 +42,30 @@ public class Main {
 		
 		User user = new User(firstName, lastName, age); //user object
 		
+		
+		while(!input.equals("-1")) { //TODO: wont let you go back into the switch statement after leaving!!!
 		System.out.println("Welcome, " + firstName + " " + lastName + ", what would you like to do?");
-		System.out.println("1: Access BIPOC Mental Health Resources");
+		System.out.println("1: Access BIPOC Mental Health Resources\n\n-1 To Exit");				
 		
 		input = scnr.next();
 		
 		switch(input)
 		{
 		case "1":
+		while(!resourceInput.equals("-1"))
+		{
+			System.out.println("Select Resource:\n1.Articles and Information\n2. Safe Space Forums\n3.Find a Provider\n\nType -1 To Go Back");
+			resourceInput = scnr.next();
+			if(resourceInput.equals("1"))
+			object.print(ArticleInfo);
+			
+			if(resourceInput.equals("2"))
+			object.print(SafeSpaceList);
+			
+			if(resourceInput.equals("3"))
+			object.print(ProviderList);
+		}
 		
-		object.print(mentalHealthTotal);
 			
 		break;
 		case "2":
@@ -60,7 +81,7 @@ public class Main {
 		//etc
 		}				//main menu as a switch statement ^
 		
-	
+		}//end while
 	}
 
 }
