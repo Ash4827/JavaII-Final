d0a9ea74e66c413751f32a0005ba9f9cd242eccf
ashton <ashton@DESKTOP-HOPMU83>
2026-03-04 01:52:34 -0800

    added basic historical figure functionality

--- src/axhopf/Main.java
+++ src/axhopf/Main.java
@@ -5,28 +5,26 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static ArrayList<Information> conversion(List<String> data)
-	{
-		ArrayList<Information> list = new ArrayList<>();
-		for(String item : data)
-		{
-			list.add(new Information(item));
-		}
-		
-		return list;
-	}
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Manager manager = new Manager();
+		
 		List<String> ArticlesInfo = List.of("Mental Health America\nhttps://mhanational.org/bipoc-mental-health/", "NAMI Seattle Mental Health Resources\nhttps://namiseattle.org/support-resources/bipoc-mental-health-resources/");
 		List<String> SafeSpaces = List.of("Black Emotional and Mental Health Collective\nhttps://wellness.beam.community/", "BIPOC Adoptees Support\nhttps://bipocadoptees.space/forms/submit-resource");
 		List<String> Providers = List.of("University of Washington BIPOC Resources\nhttps://geography.washington.edu/community-sourced-resources-bipoc-mental-health");
-		ArrayList<Information> ArticleInfo = conversion(ArticlesInfo);
-		ArrayList<Information> SafeSpaceList = conversion(SafeSpaces);
-		ArrayList<Information> ProviderList = conversion(Providers);
+		ArrayList<Resource> ArticleInfo = manager.conversion(ArticlesInfo);
+		ArrayList<Resource> SafeSpaceList = manager.conversion(SafeSpaces);
+		ArrayList<Resource> ProviderList = manager.conversion(Providers);
+		
+		HistoricalFigure Coachman = new HistoricalFigure("Alice Coachman", "Gold Medalist", 1948, "Was denied multiple times to compete in the olympics; Even so, she ran barefoot on dirt roads and used sticks to practice her high jump, eventually catching the attention of Tuskegee Institute and broke records.", "Bonn, T. (2026, February 1). 8 Black pioneers who were somehow overlooked in the history books. Katie Couric Media. https://katiecouric.com/entertainment/little-known-black-historical-figures/");
+		
+		manager.addFigure(Coachman);
 		
 		DiversityCalculator calcIndex = new DiversityCalculator();
-		Information object = new Information();
+		
 		Scanner scnr = new Scanner(System.in);
 		String input = "";
 		String resourceInput;
@@ -45,12 +43,12 @@ public class Main {
 		System.out.println("Enter Your Age:");
 		age = scnr.nextInt();
 		
-		User user = new User(firstName, lastName, age); //user object if needed
+		
 		
 		
 		while(!input.equals("-1")) { 
 		System.out.println("Welcome, " + firstName + " " + lastName + ", what would you like to do?");
-		System.out.println("1: Access BIPOC Mental Health Resources\n2: Calculate Diversity in Any Group\n\n-1 To Exit");				
+		System.out.println("1: Access BIPOC Mental Health Resources\n2: Calculate Diversity in Any Group\n3: View Historical Figures\n\n-1 To Exit");				
 		
 		input = scnr.next();
 		resourceInput = "";
@@ -62,13 +60,13 @@ public class Main {
 			System.out.println("Select Resource:\n1.Articles and Information\n2. Safe Space Forums\n3.Find a Provider\n\nType -1 To Go Back");
 			resourceInput = scnr.next();
 			if(resourceInput.equals("1"))
-			object.print(ArticleInfo);
+			manager.print(ArticleInfo);
 			
 			if(resourceInput.equals("2"))
-			object.print(SafeSpaceList);
+			manager.print(SafeSpaceList);
 			
 			if(resourceInput.equals("3"))
-			object.print(ProviderList);
+			manager.print(ProviderList);
 		}
 		
 			
@@ -89,7 +87,7 @@ public class Main {
 		break;
 		case "3":
 		
-		//something here
+		manager.printFigures(); //TODO add more historical figures, allow user to search for a specific figure
 			
 		break;
 		default:
