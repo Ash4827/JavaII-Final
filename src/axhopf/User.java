6faa97cddc2a8f5291822aa5c13c976ef3abde9f
ashton <ashton@DESKTOP-HOPMU83>
2026-03-04 23:16:19 -0800

    added back user class

---
+++ src/axhopf/User.java
@@ -0,0 +1,38 @@
+package axhopf;
+
+public class User {
+	
+	private String name;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public User()
+	{
+		setName(name);
+		setAge(age);
+	}
+	
+	public User(String name, int age)
+	{
+		setName(name);
+		setAge(age);
+	}
+	
+	@Override
+    public String toString() {
+     return "|" + name + ", Age: " + age + "|";
+    }
+
+}
