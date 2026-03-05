829644c5b6bc3b06adb3ac113c96aae6382ef728
ashton <ashton@DESKTOP-HOPMU83>
2026-03-05 01:16:52 -0800

    calender class

---
+++ src/axhopf/Calender.java
@@ -0,0 +1,39 @@
+package axhopf;
+
+public class Calender{
+	
+	public void showEvents(int month)
+	{
+		switch(month) {
+		case 1:
+		System.out.println("January Events:\nBarack Obama was Inaugurated as the first Official Black U.S. President Jan 20, 2009\nPhiladelphia Blacks Push Back Against Forced Deportation, Jan. 10, 1811\nJan 3, 1621 The First Black Child Was Born In America");
+		break;
+		case 2:
+		break;
+		case 3:
+		break;
+		case 4:
+		break;
+		case 5:
+		break;
+		case 6:
+		break;
+		case 7:
+		break;
+		case 8:
+		break;
+		case 9:
+		break;
+		case 10:
+		break;
+		case 11:
+		break;
+		case 12:
+		break;
+		default:
+		System.out.println("Invalid Month");
+		break;
+		}
+	}
+
+}
